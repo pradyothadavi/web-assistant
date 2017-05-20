051674605069fe6d6ab30cd7ea72f8016963b8c2
Pradyot H Adavi <adavi.pradyot89@gmail.com>
2017-05-20 13:59:02 +0530

    Fixed JSON issues and model bump up

--- web-assistant-api/src/main/java/in/adavi/pradyot/web/assistant/api/response/FacebookResponse.java
+++ web-assistant-api/src/main/java/in/adavi/pradyot/web/assistant/api/response/FacebookResponse.java
@@ -1,15 +1,21 @@
 package in.adavi.pradyot.web.assistant.api.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Created by pradyot.ha on 19/05/17.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FacebookResponse {
   
-  @JsonProperty("data")
+  @JsonProperty("facebook")
   private Object data;
   
+  @JsonIgnore
   public Object getData() {
     return data;
   }
